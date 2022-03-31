39f38bd30b224229f5b68934fb88ac007d7905b8
hernandez-crypto <hernandezjulio58@ymail.com>
2022-03-31 17:24:11 -0500

    main function and preoperative directives added

---
+++ C++/chapter5_20.cpp
@@ -0,0 +1,17 @@
+/*
+CIS 123 701
+Julio Cesar Hernandez
+March 31th, 2022
+
+This program imports a file which contains data points for a rocket such as time, meters, meters/second, and meter/seconds^2.
+The program reads the data points and determines at which point the rocket has begun falling back to earth.
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    return 0;
+};
