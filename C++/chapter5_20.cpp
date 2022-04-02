5f0e19e055b468094352f9670f1e20750f0dc1a2
hernandez-crypto <hernandezjulio58@ymail.com>
2022-04-01 19:41:55 -0500

    added comments to code to explain whats going on, finished maybe

--- C++/chapter5_20.cpp
+++ C++/chapter5_20.cpp
@@ -13,29 +13,27 @@ The program reads the data points and determines at which point the rocket has b
 using namespace std;
 
 int main() {
-    ifstream dataFile;
-    dataFile.open("rocket1.txt");
-    if(dataFile.fail()) {
-        cout << "rocket1.txt cannot be opened.";
-        exit(1);
+    ifstream dataFile; // syntax for getting a file opened
+    dataFile.open("rocket1.txt"); // opening the rocket1.txt file..
+    if(dataFile.fail()) {  // if rocket1.txt can not be opened due to error...
+        cout << "rocket1.txt cannot be opened."; // print out to the console this string
+        exit(1); // exit the program
     };
 
-    int n(0);
-    dataFile >> n;
+    int n(0); // n = number of entries
+    dataFile >> n; // read the first line in the rocket1.txt file to determine number of entries
 
-    double time(0), altitude(0), velocity(0), acceleration(0);
-    double currentAlt(0), difference(0), x(0);
+    double time(0), altitude(0), velocity(0), acceleration(0); // initialize variables of time altitude velocity and acceleration to double's set to 0
+    double currentAlt(0), difference(0); // initialize variables of currentAlt and difference (of altitude and current altitude) to double's set to 0
 
-    for(int i = 0; i < n; i++) {
-        dataFile >> time >> altitude >> velocity >> acceleration;
-        difference = altitude - currentAlt;
-        if (difference < 0) {
-            cout << "Time at which rocket starts returning to Earth: " << time << endl;
-            break; 
+    for (int i = 0; i < n; i++) {  // start a for loop that runs until i > n
+        dataFile >> time >> altitude >> velocity >> acceleration; // temporarily map values from rocket1.txt onto these variables we have set 
+        difference = altitude - currentAlt; // calculate the difference of altitude and current altitude to determine if the rocket had begun to fall
+        if (difference < 0) { // if the rocket has begun to fall 
+            cout << "Time at which rocket starts returning to Earth: " << time << endl; // print out the time onto the console
+            break; // end for loop early
         }
-        currentAlt = altitude;
+        currentAlt = altitude; // set currentAlt to altitude if the rocket has not begun to fall
     };
-
-
-    return 0;
+    return 0;  // end main by returning int 0
 };
