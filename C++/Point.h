a2a3d4a817f59ed252ce8fe3f1c73dcf1cbc42bc
hernandez-crypto <hernandezjulio58@ymail.com>
2022-04-07 14:30:51 -0500

    added Point.h

---
+++ C++/Point.h
@@ -0,0 +1,34 @@
+/*
+CIS 123 701 
+April 7th 2022
+
+Class for assignments in week 10 homework
+
+*/
+
+class Point 
+{
+    // type declaration statements
+    private:
+        double xCord, yCord; // data members
+    public:
+    // class methods
+        Point(); // default constructor
+        Point(double x, double y); // parametized constructor
+        // overload operators
+        double operator - (const Point& rhs) const;
+        bool operator == (const Point& rhs) const;
+        // accessor method definitions
+        double getX() const
+        {
+            return xCord;
+        };
+        double getY() const
+        {
+            return yCord;
+        };
+        // mutator methods
+        void setX(double newX);
+        void setY(double newY);
+
+}
