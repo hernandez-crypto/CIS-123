d6f36d2a7360127e4b568cd36d3edea4f7cc98ed
hernandez-crypto <hernandezjulio58@ymail.com>
2022-04-27 16:14:40 -0500

    fully functional

--- C++/chapter9/chapter9_7.cpp
+++ C++/chapter9/chapter9_7.cpp
@@ -11,55 +11,29 @@ This program determines the number of times a specified character appears in an 
 using namespace std;
 
 // function prototypes
-void stringupper(char*&);
-int countchar(char*&, char);
 
 int main()
 {
     // declare and initialize objects
     char strg1[] = "abbcfgwbibbw";
     char *ptr_strg1 = strg1, ch = 'B';
+    int cnt(0);
 
     // convert string to uppercase
-    stringupper(ptr_strg1);
-
+    while(*ptr_strg1)
+    { 
+        // convert character to uppercase
+        *ptr_strg1 = toupper(*ptr_strg1);
+        // if current character matches ch increment by one
+        if(*ptr_strg1 == ch) cnt++;
+        ptr_strg1++;
+    }
 
     cout << "The letter " << ch << " appears "
-         << countchar(ptr_strg1, ch) << " times in the string "
+         << cnt << " times in the string "
          << ptr_strg1 << endl;
     
     // end main function
     return 0;
 }
 
-// this function counts the number of times the character ch appears in the string pointed to by the ptr_strg
-int countchar(char* &ptr_strg, char ch)
-{
-    // declare and initialize objects
-    int cnt(0);
-
-    cout << *ptr_strg << endl;
-    // while not end of string
-    while(*ptr_strg)
-    {
-        // look for ch and increment cnt
-        if(*ptr_strg == ch) cnt++;
-
-        // move pointer to next character
-        ptr_strg++;
-    }
-
-    return cnt;
-}
-
-// this function converts each character in the string pointed to by ptr_strg to uppercase
-void stringupper(char* &ptr_strg)
-{
-    // while not end of string
-    while(*ptr_strg)
-    {
-        // convert character to uppercase
-        *ptr_strg = toupper(*ptr_strg);
-        ptr_strg++;
-    }
-}
